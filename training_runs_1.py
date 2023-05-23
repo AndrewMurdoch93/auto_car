87dc141ed3a7f5d81840679f20b57214cb71e3f3
Andrew Murdoch <andrew.murdoch93@gmail.com>
2023-05-23 17:16:03 +0200

    new training run

--- training_runs_1.py
+++ training_runs_1.py
@@ -176,6 +176,129 @@ main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, de
 # main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
 
 
+agent_name = 'f1_esp_pete_f_33'
+main_dict['name'] = agent_name
+env_dict['map_name'] = 'f1_esp'
+env_dict['control_steps'] = 3
+# env_dict['reward_signal']['collision']
+a = main_multiple.trainingLoop(main_dict, agent_td3_dict, env_dict, load_agent='')
+a.train()
+main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
+
+
+
+agent_name = 'f1_esp_pete_r_p_1'
+main_dict['name'] = agent_name
+main_dict['runs'] = 3
+env_dict['map_name'] = 'f1_esp'
+env_dict['control_steps'] = 10
+env_dict['reward_signal']['collision'] = -1
+a = main_multiple.trainingLoop(main_dict, agent_td3_dict, env_dict, load_agent='')
+a.train()
+main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
+
+agent_name = 'f1_esp_pete_r_p_5'
+main_dict['name'] = agent_name
+main_dict['runs'] = 3
+env_dict['map_name'] = 'f1_esp'
+env_dict['control_steps'] = 10
+env_dict['reward_signal']['collision'] = -5
+a = main_multiple.trainingLoop(main_dict, agent_td3_dict, env_dict, load_agent='')
+a.train()
+main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
+
+
+agent_name = 'f1_esp_pete_r_d_2'
+main_dict['name'] = agent_name
+main_dict['runs'] = 3
+env_dict['map_name'] = 'f1_esp'
+env_dict['control_steps'] = -2
+env_dict['reward_signal']['distance'] = 0.2
+a = main_multiple.trainingLoop(main_dict, agent_td3_dict, env_dict, load_agent='')
+a.train()
+main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
+
+agent_name = 'f1_esp_pete_r_d_25'
+main_dict['name'] = agent_name
+main_dict['runs'] = 3
+env_dict['map_name'] = 'f1_esp'
+env_dict['control_steps'] = -2
+env_dict['reward_signal']['distance'] = 0.25
+a = main_multiple.trainingLoop(main_dict, agent_td3_dict, env_dict, load_agent='')
+a.train()
+main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
+
+
+agent_name = 'f1_esp_pete_bs_200'
+main_dict['name'] = agent_name
+main_dict['runs'] = 3
+env_dict['map_name'] = 'f1_esp'
+env_dict['control_steps'] = -2
+env_dict['reward_signal']['distance'] = 0.3
+agent_td3_dict['batch_size'] = 200
+a = main_multiple.trainingLoop(main_dict, agent_td3_dict, env_dict, load_agent='')
+a.train()
+main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
+
+
+agent_name = 'f1_esp_pete_bs_600'
+main_dict['name'] = agent_name
+main_dict['runs'] = 3
+env_dict['map_name'] = 'f1_esp'
+env_dict['control_steps'] = -2
+env_dict['reward_signal']['distance'] = 0.3
+agent_td3_dict['batch_size'] = 600
+a = main_multiple.trainingLoop(main_dict, agent_td3_dict, env_dict, load_agent='')
+a.train()
+main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
+
+
+agent_name = 'f1_esp_pete_nn_plus'
+main_dict['name'] = agent_name
+main_dict['runs'] = 3
+env_dict['map_name'] = 'f1_esp'
+env_dict['control_steps'] = -2
+env_dict['reward_signal']['distance'] = 0.3
+agent_td3_dict['batch_size'] = 400
+agent_td3_dict['layer1_size'] = 500
+agent_td3_dict['layer1_size'] = 400
+a = main_multiple.trainingLoop(main_dict, agent_td3_dict, env_dict, load_agent='')
+a.train()
+main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
+
+agent_name = 'f1_esp_pete_nn_minus'
+main_dict['name'] = agent_name
+main_dict['runs'] = 3
+env_dict['map_name'] = 'f1_esp'
+env_dict['control_steps'] = -2
+env_dict['reward_signal']['distance'] = 0.3
+agent_td3_dict['batch_size'] = 400
+agent_td3_dict['layer1_size'] = 300
+agent_td3_dict['layer1_size'] = 200
+a = main_multiple.trainingLoop(main_dict, agent_td3_dict, env_dict, load_agent='')
+a.train()
+main_multiple.lap_time_test_with_noise(agent_name=agent_name, n_episodes=100, detect_issues=False, initial_conditions=True, noise={'xy':0.025, 'theta':0.05, 'v':0.1, 'lidar':0.01})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # agent_name = 'f1_esp_pete_f_50'
 # main_dict['name'] = agent_name
 # env_dict['map_name'] = 'esp'
